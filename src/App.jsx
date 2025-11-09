8998c5ee717fca43a47df077655518977e98f0c6
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-09 11:08:40 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import Rules from "./pages/Rules.jsx";
 import Legal from "./pages/Legal.jsx";
-import "./theme.css";
 
 export default function App() {
   return (
     <div className="wrap">
-      <h1>Gripetime</h1>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/rules">Rules</Link> |{" "}
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/rules">Rules</Link>
         <Link to="/legal">Legal</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<div>Welcome to Gripetime!</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/rules" element={<Rules />} />
         <Route path="/legal" element={<Legal />} />
       </Routes>
