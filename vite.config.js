e21a914d89dc5bae4965300de83d85bd71c2b4d3
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-10 16:25:22 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Gripetime.com/",  // 👈 ADD THIS LINE
+  base: "/Gripetime.com/",   // 👈 important for GitHub Pages subpath
   plugins: [react()],
 });
