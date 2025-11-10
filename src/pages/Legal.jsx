217cc35033b2ab2ee5b6bb565f1420bcd05795c1
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-10 14:57:17 -0700

    Update Legal.jsx

--- src/pages/Legal.jsx
+++ src/pages/Legal.jsx
@@ -1,4 +1,11 @@
-export default function Legal() {
-  return <h1>Legal</h1>;
-}
+// src/pages/Legal.jsx
+import React from "react";
 
+export default function Legal() {
+  return (
+    <section className="card">
+      <h2>Legal</h2>
+      Gripetime is for entertainment only, not legal advice.
+    </section>
+  );
+}
