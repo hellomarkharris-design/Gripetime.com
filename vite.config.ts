9f8ac76dfa8e2e76bd7e8d00509d589898c250c4
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-11 10:42:54 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,7 @@
-[33m9c02ce6[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Update vite.config.js
-[33mcaa3bd9[m Update App.jsx
-[33m706eac7[m Update App.jsx
-[33m27f5bd6[m Update App.jsx
-[33m43866a1[m Update App.jsx
-[33m57e553a[m Create Header
-[33m27fa1ae[m Update App.jsx
-[33m144b915[m Add files via upload
-[33m2c4f15c[m Create .gitkeep
-[33me1a58b0[m Delete src/assets
-[33m00b796f[m Create assets
-[33m9bb5294[m Add files via upload
-[33mec4d629[m Add files via upload
-[33me08a431[m Update styles.css
-[33m0c0c766[m Delete src/styles
-[33ma676e8d[m Update README.md
-[33mb06a976[m Update main.jsx
-[33m1d28979[m Create styles
-[33m3161beb[m Update Sidebar.jsx
-[33m9cf4493[m Create HeadtoHead.jsx
-[33m29b6e02[m Update Rules.jsx
-[33mbfcbb9e[m Update Respond.jsx
-[33m217cc35[m Update Legal.jsx
-[33mb54194d[m Create Leaderboards.jsx
-[33m0692801[m Update Gripes.jsx
-[33m89c68e5[m Update Create.jsx
-[33m5282a77[m Create Auth.jsx
-[33mc362495[m Rename Admin to Admin.jsx
-[33mdf45f9b[m Create Admin
-[33m415a29a[m Update Home.jsx
-[33m2e163ee[m Update vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/Gripetime.com/",
+  plugins: [react()],
+});
