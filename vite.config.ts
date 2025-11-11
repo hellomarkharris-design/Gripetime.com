d38dfcbdf458232d20821015a80e6b04a6e6442d
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-11 10:51:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Gripetime.com/",
+  base: "/Gripetime.com/", // must match repo name exactly (case-sensitive)
   plugins: [react()],
 });
