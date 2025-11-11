e622791d5453f5b1e339c276b86be5ab7d04676d
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-11 10:42:19 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/Gripetime.com/",    // 👈 MUST match your repo name exactly
-  plugins: [react()],
-});
