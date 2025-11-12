cd63bf0a45fcb0c55057fd9598d3c3692cba540a
hellomarkharris-design <hellomarkharris@gmail.com>
2025-11-12 13:44:48 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: this must match your repo name exactly (case-sensitive)
 export default defineConfig({
   plugins: [react()],
-  base: '/Gripetime.com/', // EXACT repo name, case-sensitive
+  base: '/Gripetime.com/',
 })
